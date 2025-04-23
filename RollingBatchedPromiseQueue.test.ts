6145a777da0411e2387ccc66f066f45cab3a03fc
elturpin <laurent.turpin@mailo.com>
2025-04-23 15:18:43 +0200

    tests: RollingBatched handles rejections

--- RollingBatchedPromiseQueue.test.ts
+++ RollingBatchedPromiseQueue.test.ts
@@ -56,6 +56,28 @@ describe('RollingBatchedPromiseQueue', () => {
         expect(task4).toHaveBeenCalled();
     });
 
+    it('should execute the next batch if the first is rejected', async () => {
+        const queue = new RollingBatchedPromiseQueue(2);
+        const { task: task1, reject: reject1 } = createTestTask();
+        const { task: task2, reject: reject2 } = createTestTask();
+        const { task: task3 } = createTestTask();
+        const { task: task4 } = createTestTask();
+        const result1 = queue.enqueue(task1);
+        const result2 = queue.enqueue(task2);
+        queue.enqueue(task3);
+        queue.enqueue(task4);
+
+        reject1(1);
+        reject2(2);
+        await expect(result1).rejects.toBe(1);
+        await expect(result2).rejects.toBe(2);
+
+        await setTimeout(WAIT_TIME);
+
+        expect(task3).toHaveBeenCalled();
+        expect(task4).toHaveBeenCalled();
+    });
+
     it('should execute the third task only if the first task is resolved', async () => {
         const queue = new RollingBatchedPromiseQueue(2);
         const { task: task1, resolve: resolve1 } = createTestTask();
@@ -72,5 +94,23 @@ describe('RollingBatchedPromiseQueue', () => {
 
         expect(task3).toHaveBeenCalled();
         expect(task4).not.toHaveBeenCalled();
+    });
+
+    it('should execute the forth task only if the second task is resolved', async () => {
+        const queue = new RollingBatchedPromiseQueue(2);
+        const { task: task1 } = createTestTask();
+        const { task: task2, resolve: resolve2 } = createTestTask();
+        const { task: task3 } = createTestTask();
+        const { task: task4 } = createTestTask();
+        queue.enqueue(task1);
+        queue.enqueue(task2);
+        queue.enqueue(task3);
+        queue.enqueue(task4);
+
+        resolve2(1);
+        await setTimeout(WAIT_TIME);
+
+        expect(task3).not.toHaveBeenCalled();
+        expect(task4).toHaveBeenCalled();
     });
 });
