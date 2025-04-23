9927170812720cfdca153dddd7affc3fba23e621
elturpin <laurent.turpin@mailo.com>
2025-04-23 15:23:06 +0200

    refactor: return a promise factory in Dynamic queue

--- DynamicBatchedPromiseQueue.ts
+++ DynamicBatchedPromiseQueue.ts
@@ -14,14 +14,17 @@ export class DynamicBatchedPromiseQueue implements IPromiseQueue {
     }
 
     async enqueue<T>(callback: PromiseCreator<T>): Promise<T> {
-        let result: Promise<T>;
+        const afterSelectingWorker = this.operationSerializer.enqueue(
+            async () => {
+                const queueIndex = await Promise.race(this.queueRacers);
+                const result = callback();
+                this.queueRacers[queueIndex] = result.then(() => queueIndex);
+                return () => result;
+            },
+        );
 
-        const later = this.operationSerializer.enqueue(async () => {
-            const queueIndex = await Promise.race(this.queueRacers);
-            result = callback();
-            this.queueRacers[queueIndex] = result.then(() => queueIndex);
-        });
-
-        return later.then(() => result);
+        return afterSelectingWorker.then((promiseReturner) =>
+            promiseReturner(),
+        );
     }
 }
